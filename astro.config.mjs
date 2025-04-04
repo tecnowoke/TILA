d64bfd0020089f1716f1c48138f6bbe13ea4e092
solsticiodeverano <talleresfinge@gmail.com>
2025-04-03 22:42:14 -0300

    links ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://tecnowoke.github.io",
-    base: "TILA",
+    base: "TILA/",
     output: "static",
 
 });
